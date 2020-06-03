cf15fee9ab40bd34a9ab9c1879b85fecf9e4b4a7
Rishi Malhotra <rishirmalhotra@gmail.com>
2020-06-03 01:30:21 -0400

    reporter update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # monthly-sales
 
 #https://github.com/prof-rossetti/intro-to-python/blob/master/exercises/monthly-sales-reporting/README.md
+cd /Users/Rishi/Documents/GitHub/monthly-sales
+
+conda create -n sales-env python=3.7 # (first time only)
+conda activate sales-env
+pip install pandas # (only if you're using pandas to parse the CSV files)
+python reporter.py
--- reporter.py
+++ reporter.py
@@ -1,15 +1,29 @@
 # reporter.py
 
-def to_usd(my_price):
-    """
-    Converts a numeric value to usd-formatted string, for printing and display purposes.
+import os
+import pandas
+#csv_filepath = "data/products.csv"
+#csv_filepath = os.path.join(os.path.dirname(__file__), "..", "sales-201710.csv")
+sales = pandas.read_csv("sales-201710.csv")
 
-    Param: my_price (int or float) like 4000.444444
+#breakpoint()
+
+
+df = pandas.read_csv(csv_filepath)
+print(type(df)) #> <class 'pandas.core.frame.DataFrame'>
+print(df.head()) #> prints first few rows
+products = df.to_dict("records")
+
+#def to_usd(my_price):
+    # """
+    # Converts a numeric value to usd-formatted string, for printing and display purposes.
+
+    # Param: my_price (int or float) like 4000.444444
     
-    Example: to_usd(4000.444444)
+    # Example: to_usd(4000.444444)
     
-    Returns: $4,000.44
-    """
-    return f"${my_price:,.2f}" #> $12,000.71
+    # Returns: $4,000.44
+    # """
+    # return f"${my_price:,.2f}" #> $12,000.71
 
 print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
